9bdc0ce18af8a09d035fb60403331a35a9a22be2
Nikolai Sivertsen <nikolai.sivertsen@gmail.com>
2017-02-19 23:05:16 +0100

    replace XMLHttpRequest with fetch()

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,14 @@
 import Application from './app';
 import Player from './player';
-import {DOMready, getJSON} from './util';
+import {DOMready, parseJSON, checkStatus} from './util';
 
 DOMready(() => {
-    getJSON('{{BASE_URL}}/data.json', (data) => {
+    fetch('{{BASE_URL}}/data.json')
+      .then(checkStatus)
+      .then(parseJSON)
+      .then(data => {
         let spots = document.getElementsByClassName('spot');
         let pages = document.getElementsByClassName('content-page');
         let application = new Application(spots, pages, data);
-    });
+      }).catch(error => console.log('request failed', error));
 });
--- src/scripts/util.js
+++ src/scripts/util.js
@@ -32,6 +32,20 @@ export function getJSON(url, cb) {
     request.send();
 }
 
+export function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
+  } else {
+    var error = new Error(response.statusText)
+    error.response = response
+    throw error
+  }
+}
+
+export function parseJSON(response) {
+  return response.json()
+}
+
 export function each(arr, cb) {
     for(let i = 0; i < arr.length; i++) {
         cb(arr[i], i, arr);
